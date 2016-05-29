88545deda4bccf3f7b616580b82daf5e94af5abd
haohangan <1242188036@qq.com>
2016-05-30 01:56:49 +0800

    scala second

--- article/scala/main.scala
+++ article/scala/main.scala
@@ -3,3 +3,6 @@ https://wizardforcel.gitbooks.io/w3school-scala/content/21.html
 
 https://twitter.github.io/scala_school/zh_cn/basics.html
 再看
+
+https://twitter.github.io/scala_school/zh_cn/
+2016-30 开始
