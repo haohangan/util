e65c56be0317529ad94a86cff37fed59baf67d2e
haohangan <1242188036@qq.com>
2016-06-15 00:09:14 +0800

    test pool

---
+++ base/pool/self/test.java
@@ -0,0 +1,21 @@
+public class Main {
+  public static void main(String args[]) {
+    // Do something...
+    //     ...
+    //
+    //         // Create the ConnectionPool:
+    //             JDBCConnectionPool pool = new JDBCConnectionPool(
+    //                   "org.hsqldb.jdbcDriver", "jdbc:hsqldb://localhost/mydb",
+    //                         "sa", "secret");
+    //
+    //                             // Get a connection:
+    //                                 Connection con = pool.checkOut();
+    //
+    //                                     // Use the connection
+    //                                         ...
+    //
+    //                                             // Return the connection:
+    //                                                 pool.checkIn(con);
+    //                                                  
+    //                                                    }
+    //                                                    }
