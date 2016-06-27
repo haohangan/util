dac7f2d0bcb92deabacb70bca6bdbe6bcf51b6ad
haohangan <1242188036@qq.com>
2016-06-27 10:06:05 +0800

    mongodb page

---
+++ article/mongodb/分页查询.js
@@ -0,0 +1,21 @@
+db.getCollection('messages').find({});
+
+//È¥³ýÄ³Ð©×Ö¶Î
+db.getCollection('messages').find(
+{'bussId':'XMMXMXMXMX'},
+{'details':0}
+);
+
+//·ÖÒ³
+db.getCollection('messages').find(
+{'bussId':'XMMXMXMXMX'},
+{'details':{'$slice':[0,50]}}
+);
+
+Query query = new Query();
+query.addCriteria(Criteria.where("chatId").is("1"));
+query.fields().slice("details", 0, 5);
+DBMessage d = template.findOne(query, DBMessage.class);
+System.out.println(d.getDetails().size());
+
+
