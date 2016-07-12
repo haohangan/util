0cc7eba080c23d2367d024392cc588035d6998d9
haohangan <1242188036@qq.com>
2016-07-12 19:28:27 +0800

    key

---
+++ base/publickey/LoadKey.java
@@ -0,0 +1,135 @@
+
+package com.grgbanking.api.utils;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import org.bouncycastle.openssl.PEMReader;
+import org.bouncycastle.util.io.pem.PemObject;
+
+public class LoadKey {
+	private static PEMReader reader;
+
+	/**
+	 * D:\\aptoto_public_key.pem
+	 * 
+	 * @param path
+	 * @return
+	 * @throws IOException
+	 */
+	public static byte[] readPem(String path) throws IOException {
+		reader = new PEMReader(new FileReader(new File(path)));
+		PemObject obj = reader.readPemObject();
+		byte[] keyByte = obj.getContent();
+		return keyByte;
+	}
+
+	/**
+	 * 从字符串中加载公钥
+	 * 
+	 * @param publicKeyStr
+	 *            公钥数据字符串
+	 * @throws Exception
+	 *             加载公钥时产生的异常
+	 */
+	public static RSAPublicKey loadPublicKeyByStr(byte[] buffer) throws Exception {
+		try {
+			// byte[] buffer = Base64.decode(publicKeyStr);
+			// byte[] buffer = Base64.decodeBase64(publicKeyStr);
+			KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+			X509EncodedKeySpec keySpec = new X509EncodedKeySpec(buffer);
+			return (RSAPublicKey) keyFactory.generatePublic(keySpec);
+		} catch (NoSuchAlgorithmException e) {
+			throw new Exception("无此算法");
+		} catch (InvalidKeySpecException e) {
+			throw new Exception("公钥非法");
+		} catch (NullPointerException e) {
+			throw new Exception("公钥数据为空");
+		}
+	}
+
+	/**
+	 * 公钥加密过程
+	 * 
+	 * @param publicKey
+	 *            公钥
+	 * @param plainTextData
+	 *            明文数据
+	 * @return
+	 * @throws Exception
+	 *             加密过程中的异常信息
+	 */
+	public static byte[] encrypt(RSAPublicKey publicKey, byte[] plainTextData) throws Exception {
+		if (publicKey == null) {
+			throw new Exception("加密公钥为空, 请设置");
+		}
+		Cipher cipher = null;
+		try {
+			// 使用默认RSA
+			cipher = Cipher.getInstance("RSA");
+			// cipher= Cipher.getInstance("RSA", new BouncyCastleProvider());
+			cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+			byte[] output = cipher.doFinal(plainTextData);
+			return output;
+		} catch (NoSuchAlgorithmException e) {
+			throw new Exception("无此加密算法");
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+			return null;
+		} catch (InvalidKeyException e) {
+			throw new Exception("加密公钥非法,请检查");
+		} catch (IllegalBlockSizeException e) {
+			throw new Exception("明文长度非法");
+		} catch (BadPaddingException e) {
+			throw new Exception("明文数据已损坏");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		String str = "道场成就，征集江城、斋主虔诚，上香设拜。abcdefgh";
+		byte[] plainTextData = str.getBytes();
+		RSAPublicKey publickey = loadPublicKeyByStr(readPem("D:\\aptoto_public_key.pem"));
+		byte[]  after = encrypt(publickey,plainTextData);
+		System.out.println(new String(after));
+		
+	}
+	
+	/** 
+     * 字节数据转字符串专用集合 
+     */  
+    private static final char[] HEX_CHAR = { '0', '1', '2', '3', '4', '5', '6',  
+            '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };  
+	
+	/** 
+     * 字节数据转十六进制字符串 
+     *  
+     * @param data 
+     *            输入数据 
+     * @return 十六进制内容 
+     */  
+    public static String byteArrayToString(byte[] data) {  
+        StringBuilder stringBuilder = new StringBuilder();  
+        for (int i = 0; i < data.length; i++) {  
+            // 取出字节的高四位 作为索引得到相应的十六进制标识符 注意无符号右移  
+            stringBuilder.append(HEX_CHAR[(data[i] & 0xf0) >>> 4]);  
+            // 取出字节的低四位 作为索引得到相应的十六进制标识符  
+            stringBuilder.append(HEX_CHAR[(data[i] & 0x0f)]);  
+            if (i < data.length - 1) {  
+                stringBuilder.append(' ');  
+            }  
+        }  
+        return stringBuilder.toString();  
+    }  
+}
