8aacae54de8a70bf364188430159a2f5029900db
YKSE <YKSE>
2016-10-27 17:09:29 +0800

    merge dbutils

--- dbutils/utils/src/test/test.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-import com.eva.core.db.baseutils.DBConnection;
-
-public class test {
-    public static void main(String[] args) {
-		System.out.println(DBConnection.getConn());
-	}
-}
