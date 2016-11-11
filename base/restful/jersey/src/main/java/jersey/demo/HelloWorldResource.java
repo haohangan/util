0e3de5c8a4ecff15589b05e67569c0c1ef7b769b
YKSE <YKSE>
2016-11-11 18:39:30 +0800

    jersey servlet

---
+++ base/restful/jersey/src/main/java/jersey/demo/HelloWorldResource.java
@@ -0,0 +1,14 @@
+package jersey.demo;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("helloworld")
+public class HelloWorldResource {
+	 @GET
+	    @Produces("text/plain")
+	    public String getHello() {
+	        return "Hello World!";
+	    }
+}
---
+++ base/restful/jersey/src/main/java/jersey/demo/MyApplication.java
@@ -0,0 +1,18 @@
+package jersey.demo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class MyApplication extends Application {
+	@Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<Class<?>>();
+        // register root resource
+        classes.add(HelloWorldResource.class);
+        return classes;
+    }
+}
