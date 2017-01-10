eee6c17ccaf0991b0bdba0df54d0b9e5badce4ad
YKSE <YKSE>
2017-01-10 18:15:48 +0800

    change

--- base/netty/websocket/page/WebsocketJ.js
+++ base/netty/websocket/page/WebsocketJ.js
@@ -9,6 +9,7 @@ var status = -1;
 
 function conn(){
 	websocket = new WebSocket("ws://127.0.0.1/websocket?uname=tom","zookeeperWS");
+	console.info('开始连接');
 	websocket.onopen = function(){
 		console.info("打开websocket.");
 		status = 1;
@@ -28,6 +29,27 @@ function send(message){
 	websocket.send(message);
 }
 
+function sendBin(message){
+	var binary = new Uint8Array(message.length);
+	for (var i = 0; i < message.length; i++) {
+	binary[i] = message.charCodeAt(i);
+    }
+	console.info(binary.byteLength);
+	websocket.send(binary.buffer);
+}
+
 function closeWebsocket(){
     websocket.close();
+}
+
+function b64EncodeUnicode(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+        return String.fromCharCode('0x' + p1);
+    }));
+}
+
+function b64DecodeUnicode(str) {
+    return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
 }
---
+++ base/netty/websocket/page/WebsocketJReWrite.js
@@ -0,0 +1,50 @@
+/**
+ * rewrite the js  
+ */
+function SocketJS(){
+	var websocket;
+	
+	this.conn = function(url){
+		websocket = new WebSocket(url,"zookeeperWS");
+		websocket.onclose = function(){
+			console.info("close websocket.");
+		};
+		websocket.onerror =  function(){
+			console.info("websocket error");
+		};
+		websocket.onmessage = function(event){
+			console.info("websocket message:"+event.data);
+		};	
+	};
+	
+	this.send = function(message){
+		websocket.send(message);
+	};
+
+	this.sendBin = function(message){
+		var text = b64EncodeUnicode(message);
+		var buffer = new ArrayBuffer(text.length);
+		var view = new DataView(buffer);
+		for(var i=0, n=text.length; i<n; i++) view.setUint8(i,text.charCodeAt(i));
+		websocket.send(buffer.buffer);
+	};
+
+	this.closeWebsocket = function(){
+		websocket.close();
+	};
+	
+}
+
+function base64tobin(base64) {
+  var text = window.atob(base64);
+  var buffer = new ArrayBuffer(text.length);
+  var view = new DataView(buffer);
+  for(var i=0, n=text.length; i<n; i++) view.setUint8(i,text.charCodeAt(i));
+  return buffer;
+}
+
+function b64EncodeUnicode(str) {
+    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+        return String.fromCharCode('0x' + p1);
+    }));
+}
