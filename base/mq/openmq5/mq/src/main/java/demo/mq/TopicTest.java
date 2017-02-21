6447c98bdcb00d489c7e85e70cc90dc645b1a13f
YKSE <YKSE>
2017-02-21 10:24:24 +0800

    mq

--- base/mq/openmq5/mq/src/main/java/demo/mq/TopicTest.java
+++ base/mq/openmq5/mq/src/main/java/demo/mq/TopicTest.java
@@ -15,63 +15,68 @@ import com.sun.messaging.ConnectionFactory;
 /**
  * @author guihao
  * @date 2017年2月17日下午5:50:19
- * @desc 
+ * @desc
  */
 public class TopicTest {
-    public static void main(String[] args) throws JMSException {
-    	String topicName = "TOPIC";
-    	read(topicName);
-    	send(topicName);
+	public static void main(String[] args) throws JMSException {
+		String topicName = "TOPIC";
+		read(topicName);
+		send(topicName);
 	}
-    
-    static void send(String topicName) throws JMSException{
-    	ConnectionFactory factory = new ConnectionFactory();
+
+	static void send(String topicName) throws JMSException {
+		ConnectionFactory factory = new ConnectionFactory();
 		Session session = null;
-		Connection conn = null ;
+		Connection conn = null;
 		try {
 			factory.setProperty(com.sun.messaging.ConnectionConfiguration.imqAddressList, "localhost:7676");
 			conn = factory.createQueueConnection();
 			conn.start();
-			session = conn.createSession(false,Session.AUTO_ACKNOWLEDGE);
+			session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
 			Topic topic = session.createTopic(topicName);
 			MessageProducer msgProducer = session.createProducer(topic);
 			msgProducer.send(session.createTextMessage("111111"));
-		}catch(JMSException e){
+		} catch (JMSException e) {
 			e.printStackTrace();
-		}finally{
+		} finally {
 			conn.close();
 			session.close();
 		}
-    }
-    
-    static void read(String topicName) throws JMSException{
-    	ConnectionFactory factory = new ConnectionFactory();
+	}
+
+	static void read(String topicName) throws JMSException {
+		ConnectionFactory factory = new ConnectionFactory();
 		Session session = null;
-		Connection conn = null ;
+		Connection conn = null;
 		try {
 			factory.setProperty(com.sun.messaging.ConnectionConfiguration.imqAddressList, "localhost:7676");
 			conn = factory.createQueueConnection();
 			conn.start();
-			session = conn.createSession(false,Session.AUTO_ACKNOWLEDGE);
+			session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
 			Topic topic = session.createTopic(topicName);
 			MessageConsumer msgConsumer = session.createConsumer(topic);
 			msgConsumer.setMessageListener(new MessageListener() {
-				
+
 				@Override
 				public void onMessage(Message message) {
-					TextMessage msg = (TextMessage) message;
-					try {
-						System.out.println("Received message: " + msg.getText());
-					} catch (JMSException e) {
-						e.printStackTrace();
+
+					if (message instanceof TextMessage) {
+						TextMessage m = (TextMessage) message;
+						try {
+							System.out.println("Reading message: " + m.getText());
+						} catch (JMSException e) {
+							e.printStackTrace();
+						}
+					} else {
+						// Handle error
 					}
 				}
 			});
-		}catch(JMSException e){
+		} catch (JMSException e) {
 			e.printStackTrace();
-		}finally{
-//			conn.close();
-//			session.close();
+		} finally {
+			// conn.close();
+			// session.close();
 		}
-    }
+	}
 }
