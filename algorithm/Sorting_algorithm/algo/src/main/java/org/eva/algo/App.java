bb86c87d0e66119aeb013b3faef8ca064357ff09
haohangan <1242188036@qq.com>
2017-04-09 23:14:00 +0800

    新建项目

---
+++ algorithm/Sorting_algorithm/algo/src/main/java/org/eva/algo/App.java
@@ -0,0 +1,13 @@
+package org.eva.algo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
