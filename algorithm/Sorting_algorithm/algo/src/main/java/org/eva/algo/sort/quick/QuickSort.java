a33cb9385f285766ec1d0c02688f4457ab4802a0
haohangan <1242188036@qq.com>
2017-04-10 21:18:56 +0800

    quick sort 修正

--- algorithm/Sorting_algorithm/algo/src/main/java/org/eva/algo/sort/quick/QuickSort.java
+++ algorithm/Sorting_algorithm/algo/src/main/java/org/eva/algo/sort/quick/QuickSort.java
@@ -1,45 +1,49 @@
 package org.eva.algo.sort.quick;
 
 import org.eva.algo.sort.common.DisorderedHelper;
+
 /**
- * ��������ʹ�÷��η���Divide and conquer����������һ�����У�list����Ϊ���������У�sub-lists����
- * ����Ϊ��
+ * ��������ʹ�÷��η���Divide and
+ * conquer����������һ�����У�list����Ϊ���������У�sub-lists���� ����Ϊ��
  * 1��������������һ��Ԫ�أ���Ϊ"��׼"��pivot����
  * 2:�����������У����бȻ�׼ֵС��Ԫ�ذڷ��ڻ�׼ǰ�棬���бȻ�׼ֵ���Ԫ�ذ��ڻ�׼���棨��ͬ�������Ե���һ�ߣ����������������֮�󣬸û�׼�ʹ������е��м�λ�á������Ϊ������partition��������
  * 3:�ݹ�أ�recursively����С�ڻ�׼ֵԪ�ص������кʹ��ڻ�׼ֵԪ�ص�����������
  * �ݹ鵽��ײ�ʱ�����еĴ�С�����һ��Ҳ�����Ѿ�������ˡ�����㷨һ�����������Ϊ��ÿ�εĵ�����iteration���У������ٻ��һ��Ԫ�ذڵ�������λ��ȥ��
+ * 
  * @author 976175665
  * @date 2017��4��9�� ����10:55:26
  */
 public class QuickSort {
 
 	public static void sort(int[] src) {
-		sort2(src, 0, src.length - 1);
+		sort(src, 0, src.length - 1);
 	}
 
 	private static void sort(int[] src, int left, int right) {
 		if (left >= right) {
 			return;
 		}
-		int mid = src[right];// ����˳��
+		// int mid = src[right];// ����˳��
+		int pivotIndex = right;
 		int i = left;
 		int j = right - 1;
 		while (i < j) {
-			while (i < j && src[i] < mid) {
+			while (i < j && src[i] < src[pivotIndex]) {
 				i++;
 			}
-			while (i < j && src[j] >= mid) {
+			while (i < j && src[j] >= src[pivotIndex]) {
 				j--;
 			}
 			if (i < j) {
 				DisorderedHelper.swap(src, i, j);
 			}
 		}
-		if (src[i] > mid) {
+		if (src[i] > src[pivotIndex]) {
 			DisorderedHelper.swap(src, i, right);
+			pivotIndex = i;
+			sort(src, pivotIndex + 1, right);
 		}
-		sort(src, left, i - 1);
-		sort(src, i + 1, right);
+		sort(src, left, pivotIndex - 1);
 	}
 
 	private static void sort2(int[] src, int left, int right) {
@@ -62,6 +66,8 @@ public class QuickSort {
 		}
 		if (src[j] < mid) {
 			DisorderedHelper.swap(src, j, left);
+		} else {
+			j--;
 		}
 		sort(src, left, j - 1);
 		sort(src, j + 1, right);
