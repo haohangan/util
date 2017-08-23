75317ffb40d1b16abdbdc2beb19470ab0a62629b
haohangan <1242188036@qq.com>
2017-08-23 17:11:56 +0800

    zookeeper pseudo cluster on windows

---
+++ article/zookeeper/集群/start.bat
@@ -0,0 +1,5 @@
+start D:\zkcluster\zk1\bin\zkServer.cmd
+start D:\zkcluster\zk2\bin\zkServer.cmd
+start D:\zkcluster\zk3\bin\zkServer.cmd
+start D:\zkcluster\zk4\bin\zkServer.cmd
+pause
---
+++ article/zookeeper/集群/stop.bat
@@ -0,0 +1,5 @@
+tskill 6808
+tskill 568 
+tskill 5308
+tskill 1076
+pause
