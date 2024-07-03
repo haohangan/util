e77d0d992b63b94db12fb5e53f47ee17191ea99d
桂浩 <1242188036@qq.com>
2024-07-04 00:23:16 +0800

    Create pyscipopt_demo.py

---
+++ pythondemo/pyscipopt_demo.py
@@ -0,0 +1,28 @@
+import pyscipopt
+
+## 一组决策变量
+## 一个目标函数
+## 一组决策变量的约束条件
+
+model = pyscipopt.Model("model name")
+
+## 定义一组变量
+x1 = model.addVar()
+x2 = model.addVar()
+x3 = model.addVar()
+x4 = model.addVar()
+x5 = model.addVar()
+
+## 设定目标函数
+model.setObjective(-5 * x1 - 2 * x2 - 3 * x3 + x4 - x5)
+
+## 设定决策变量的约束条件
+model.addCons(x1 + 2 * x2 + 2 * x3 + x4 == 8)
+model.addCons(3 * x1 + 4 * x2 + x3 + x5 == 7)
+
+## 求解
+model.optimize()
+sol = model.getBestSol()
+
+print(sol)
+print(f"x1: {sol[x1]}")
